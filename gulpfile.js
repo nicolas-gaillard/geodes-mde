683446011be4883463e2d8909ea7efd5b2634bf6
Nicolas Gaillard <nicolas.gaillard@etu.univ-nantes.fr>
2017-07-26 12:45:13 -0400

    add gulp + lint

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require('gulp');
+const eslint = require('gulp-eslint');
+
+const inputPaths = {
+    javascript: [
+        '*.js',
+    ],
+};
+
+gulp.task('js:lint', () => {
+    // http://eslint.org/docs/rules
+    let task = gulp
+        .src(inputPaths.javascript)
+        .pipe(eslint())
+        .pipe(eslint.format());
+
+    if (process.env.CI) {
+        task = task.pipe(eslint.failAfterError());
+    }
+
+    return task;
+});
+
+gulp.task('js:watch', () => {
+    gulp.watch(inputPaths.javascript, ['js:lint']);
+});
+
+gulp.task('default', ['js:watch']);
